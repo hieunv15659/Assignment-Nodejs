465eaecc9615816077d5287cd58609ad05bb634e
Senpai2k <hieuxautrai2002@gmail.com>
2022-08-06 22:36:22 +0700

    model product #23

---
+++ FE/src/model/product.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+    },
+    originalPrice: {
+        type: Number,
+    },
+    image: {
+        type: String,
+    },
+    saleOffPrice: {
+        type: String,
+    },
+    category: {
+        type: mongoose.ObjectId,
+        ref: 'Category'
+    },
+    feature: {
+        type: String,
+    },
+    longDesc: {
+        type: String,
+    },
+    shortDesc: {
+        type: String,
+    },
+    id: {
+        type: Number,
+    }
+});
+
+export default mongoose.model('Product', productSchema)
