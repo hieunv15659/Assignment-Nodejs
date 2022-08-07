ab40af74725495f52bf1e7a1bbf2be5131360e22
Senpai2k <hieuxautrai2002@gmail.com>
2022-08-07 22:26:08 +0700

    app category

--- FE/src/app.js
+++ FE/src/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", routerProduct);
-
+app.use("/api", routerCategory);
 // connect db
 
 mongoose.connect("mongodb://localhost:27017/nodejs", () => {
